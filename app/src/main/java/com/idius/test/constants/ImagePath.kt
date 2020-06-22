4fffdd6299c55b50b1a2d627232a284c1650ddce
Chaesung Yoon <chaesung.yoon@geappliances.com>
2020-06-23 01:48:37 +0900

    Idius Coding Test

---
+++ app/src/main/java/com/idius/test/constants/ImagePath.kt
@@ -0,0 +1,7 @@
+package com.idius.test.constants
+
+import com.idius.test.constants.ApiAddress.BASE_URL
+
+object ImagePath {
+    const val WEATHER_ICON_64 = BASE_URL + "static/img/weather/png/64/%s.png"
+}
